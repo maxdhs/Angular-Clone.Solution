b5bb4cebb07c5ec163e4f553d43db7830d111224
maxwell dubin <maxdhs@gmail.com>
2019-01-25 14:37:35 -0800

    Add multiple occurences of name-tag component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NamesComponent } from './names/names.component';
+import { NameTagComponent } from './name-tag/name-tag.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NamesComponent
+    NamesComponent,
+    NameTagComponent
   ],
   imports: [
     BrowserModule
