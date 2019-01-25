18f2bf833838643a702d9462eb10cb8b3398ad42
maxwell dubin <maxdhs@gmail.com>
2019-01-25 10:54:41 -0800

    Add first component called names

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NamesComponent } from './names/names.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NamesComponent
   ],
   imports: [
     BrowserModule
