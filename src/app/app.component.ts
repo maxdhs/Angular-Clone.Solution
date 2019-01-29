bdc904835339d74e090a373710df7ab788b3b5b0
Guest User <Guest@epicodus-118.local>
2019-01-28 16:36:18 -0800

    Add styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  body: string [] = ["If you are alone when you crack open a peanut shell and eat the peanut inside, you are the only person in the world to have ever seen that peanut.", "What are some guy secrets girls don't know about?", "Venezuela's top military envoy to the United States has defected to support the opposition leader and calls for more to follow", "Margaret Hamilton, NASA's lead software engineer for the Apollo Program, stands next to the code she wrote by hand that took Humanity to the moon in 1969.", "Who needs an alarm clock on Sunday morning when you have a PLANK WAR going off at the neighbors" ];
+  body1: string  = "Roger Stone claims he's been treated worse than Osama bin Laden — who was shot dead, dumped in the ocean";
 
-  imgsrc: string = "../assets/img/icon1.png";
+  imgsrc1: string = "../assets/img/icon1.png";
 
-  subreddit: string = "r/todayilearned";
+  subreddit1: string = "r/todayilearned";
+
+  body2: string  = "If you are alone when you crack open a peanut shell and eat the peanut inside, you are the only person in the world to have ever seen that peanut.";
+
+  imgsrc2: string = "../assets/img/icon2.png";
+
+  subreddit2: string = "r/Atheism";
+
+  body3: string  = "Redditors with a 9-5 job in a cubicle, how do you pretend to stay busy at work?";
+
+  imgsrc3: string = "../assets/img/icon3.png";
+
+  subreddit3: string = "r/HoldMyBeer";
+
+  body4: string  = "Y'all should stop expecting good games from EA. It's not happening Why do so many people still have hope for publishers like EA? I mean fr they were letting us down for so long now, bad games, scamming us with micro transactions and bad games, demos, etc. You people are giving people money that already know they are making bad games. They don't give a fuck about you or if you like the game. Ea has never published games for the games sake but to make money. Even with Spore back then which was made more cartoon like to be able to market it towards kids.";
+
+  imgsrc4: string = "../assets/img/icon4.png";
+
+  subreddit4: string = "r/ProgrammingHumor";
 }
